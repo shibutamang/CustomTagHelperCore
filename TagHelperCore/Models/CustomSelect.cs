5153aba695e58cdda0a48e26c0289dfb82b85712
shibutamang05 <shibutamang05@gmail.com>
2019-04-28 21:06:53 +0545

    Add project files.

---
+++ TagHelperCore/Models/CustomSelect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelperCore.Models
+{
+    public class CustomSelect
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class GetValues
+    {
+        public List<CustomSelect> GetOptions()
+        {
+            List<CustomSelect> items = new List<CustomSelect>();
+            items.Add(new CustomSelect() { Name = "Option1", Value = 1 });
+            items.Add(new CustomSelect() { Name = "Option2", Value = 2 });
+
+            return items;
+        }
+    }
+
+}
---
+++ TagHelperCore/TagHelper/SelectTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TagHelperCore.Models;
+
+namespace TagHelperCore
+{
+    [HtmlTargetElement("select-option")]
+    public class SelectTagHelper : TagHelper
+    {
+        public List<CustomSelect> Options { get; set; }
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            string content = $"<select>";
+            foreach (var item in Options)
+            {
+                content += "<option value=" + "{ item.Value}" + ">" + "{ item.Name }" + "</option>";
+            }
+            output.Content.SetHtmlContent(content);
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
